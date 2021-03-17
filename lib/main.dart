248a08104808dafaa9c71942732e18be7b8ccc9c
mahmood <mahmoodesmail97@gmail.com>
2021-03-17 01:01:50 -0700

    ios v1

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'dart:developer';
 
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hadaf/ui/views/SplashScreen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(
       EasyLocalization(
         child:  MyApp(),
